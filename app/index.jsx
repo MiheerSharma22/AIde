0d46e11ca1f4ea102592a6ad2c891c4c61c0533a
MiheerSharma22 <miheer.sharma1@gmail.com>
2024-09-29 16:10:59 +0530

    added logo in login page and changed some styles

--- app/index.jsx
+++ app/index.jsx
@@ -1,46 +1,81 @@
 import { Link } from "expo-router"; // If you're using this for navigation
-import { ImageBackground, Text, View, StyleSheet, Button } from "react-native";
-import homeScreenBG from "../assets/images/AIde-homescreen-bg.png";
+import { ImageBackground, Text, View, Image } from "react-native";
+import homeScreenBG from "@/assets/images/AIde-homescreen-bg.png";
+import logo from "@/assets/images/logo.png";
 import { FontAwesome } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useEffect, useState } from "react";
 
 export default function Index() {
+  const [showWelcomeText, setShowWelcomeText] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowWelcomeText(true);
+    }, [700]);
+  }, []);
+
   return (
+    // backgrpund image container
     <ImageBackground
       source={homeScreenBG} // You can use the imported image directly
       style={{
         flex: 1,
         width: "100%", // Ensure it stretches across the width
         height: "100%", // Ensure it stretches across the height
-        justifyContent: "center",
+        justifyContent: "space-around",
         alignItems: "center",
-        gap: "2rem",
+        gap: "5rem",
       }}
       resizeMode="cover"
     >
-      <View className="w-[80%] flex items-center gap-5">
-        <Text
-          className="text-white"
-          style={{
-            fontSize: "2rem",
-          }}
-        >
-          Welcome to AIde!
-        </Text>
+      {/*logo and welcome text */}
+      <View
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "1.25rem",
+          width: "80%",
+          transformOrigin: "center",
+        }}
+      >
+        {/* logo image */}
+        <Image source={logo} className="h-[80px]" resizeMode="contain" />
+
+        {/* welcome screen text */}
+        {showWelcomeText && (
+          <Text
+            className="text-[#7B4FFA]"
+            style={{
+              fontSize: "1rem",
+              fontWeight: "600",
+              transitionProperty: "all",
+              transitionDuration: 150,
+              fontFamily: "cursive",
+              opacity: 0.7,
+            }}
+          >
+            Your own AI helper.
+          </Text>
+        )}
       </View>
 
-      <View
+      {/* sign in with google button */}
+      <TouchableOpacity
+        activeOpacity={0.7}
         style={{
           backgroundColor: "rgba(115, 114, 115, 0.43)",
           borderRadius: "0.5rem",
           flexDirection: "row",
           alignItems: "center",
           gap: "0.5rem",
+          paddingHorizontal: 20,
+          paddingVertical: 10,
         }}
-        className="p-2"
       >
         <Text className="text-white">Google Sign IN</Text>
         <FontAwesome size={28} name="google" color="white" />
-      </View>
+      </TouchableOpacity>
     </ImageBackground>
   );
 }
