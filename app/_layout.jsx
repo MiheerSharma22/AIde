6ae95ea3c56cc37946e5bab3aa1277dd017002f9
MiheerSharma22 <miheer.sharma1@gmail.com>
2024-11-28 23:17:10 +0530

    added headerShown false for all screens in root _layout file

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,17 +3,12 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack
-    // screenOptions={{
-    //   headerStyle: {
-    //     backgroundColor: "#f4511e", // header section background color
-    //   },
-    //   headerTintColor: "#fff", // header text color
-    //   headerTitleStyle: {
-    //     fontWeight: "500", // header title font weight
-    //   },
-    // }}
+      screenOptions={{
+        headerShown: false,
+      }}
     >
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="index" />
+      <Stack.Screen name="chats" />
     </Stack>
   );
 }
