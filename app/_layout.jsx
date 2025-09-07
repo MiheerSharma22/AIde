379052c952a366d6a23199df3433c11c98760f70
MiheerSharma22 <miheer.sharma1@gmail.com>
2025-09-07 17:25:57 +0530

    added /index again in chatType stack screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -8,7 +8,7 @@ export default function RootLayout() {
       }}
     >
       <Stack.Screen name="index" />
-      <Stack.Screen name="chatType" />
+      <Stack.Screen name="chatType/index" />
       <Stack.Screen
         name="chats/index"
         options={{
